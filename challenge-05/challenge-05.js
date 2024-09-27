f9c94711e3b06f380bb63e1d3d09998b1635ff7a
Nicolas <nicolasinouealcantara@gmail.com>
2024-09-26 23:05:44 -0300

    challenge 05 finalizado

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var myVar = [3, 4, 5, 2, 7];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function returnVar(array){
+    return array;
+};
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+returnVar(myVar)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,24 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+function arrayNumber(arrayValores, numero){
+    return arrayValores[numero];
+};
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var numbersArray = [true, 2, null, Nan, 'Nicolas'];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+arrayNumber(numbersArray, 0);
+arrayNumber(numbersArray, 1);
+arrayNumber(numbersArray, 2);
+arrayNumber(numbersArray, 3);
+arrayNumber(numbersArray, 4);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,26 +55,48 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+function book(nomeLivro = undefined){
+    var livros = {
+        'Conjurador': {
+            quantidadePaginas: 200,
+            autor: 'Taran Matharu',
+            editora: 'Galera'
+        },
+        'Ajin': {
+            quantidadePaginas: 80,
+            autor: 'Gamon Sakurai',
+            editora: 'Rocco'
+        },
+        'Tokyo Ghoul': {
+            quantidadePaginas: 60,
+            autor: 'Sui Ishida',
+            editora: 'Saraiva'
+        }
+    }; 
+    if (nomeLivro === undefined){
+        return livros;
+    };
+    return livros[nomeLivro];
+};
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+"O livro " + book(Ajin) + ' tem ' + book(Ajin).quantidadePaginas + ' páginas!'
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+'O autor do livro ' + book()
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
