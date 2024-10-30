f7eafc4b7cdd260d86d66fef4f6fc9d08628f03e
Nicolas <nicolasinouealcantara@gmail.com>
2024-10-30 12:37:27 -0300

    Challenge 27 corrigido

--- aula-pratica/main.js
+++ aula-pratica/main.js
@@ -1,6 +1,22 @@
 (function(win, doc){
     'use strict';
 
-    var $main = doc.querySelector('.main');
-    console.log($main.setAttribute('data-js2', 'main-datajs'));
+    function is(obj){
+        return Object.prototype.toString.call(obj);
+    };
+
+    function isArray(obj){
+        return is(obj) === '[object Array]';
+    };
+
+    function isFunction(obj){
+        return is(obj) === '[object Function]';
+    };
+
+    function isObject(obj){
+        return is(obj) === '[object Object]';
+    };
+
+    console.log(isFunction(function(){}));
+
 })(window, document);
--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -1,21 +1,126 @@
-/*
-Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
-métodos semelhantes aos que existem no array, mas que sirvam para os
-elementos do DOM selecionados.
-Crie os seguintes métodos:
-- forEach, map, filter, reduce, reduceRight, every e some.
+(function(doc){
+    'use strict';
 
-Crie também métodos que verificam o tipo do objeto passado por parâmetro.
-Esses métodos não precisam depender de criar um novo elmento do DOM, podem
-ser métodos estáticos.
+    /*
+    Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
+    métodos semelhantes aos que existem no array, mas que sirvam para os
+    elementos do DOM selecionados.
+    Crie os seguintes métodos:
+    - forEach, map, filter, reduce, reduceRight, every e some.
 
-Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
-no objeto, como nos exemplos abaixo:
-DOM.isArray([1, 2, 3]); // true
-DOM.isFunction(function() {}); // true
-DOM.isNumber('numero'); // false
+    Crie também métodos que verificam o tipo do objeto passado por parâmetro.
+    Esses métodos não precisam depender de criar um novo elmento do DOM, podem
+    ser métodos estáticos.
 
-Crie os seguintes métodos para verificação de tipo:
-- isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
-O método isNull deve retornar `true` se o valor for null ou undefined.
-*/
+    Métodos estáticos não obrigam o uso do `new`, podendo ser usados diretamente
+    no objeto, como nos exemplos abaixo:
+    DOM.isArray([1, 2, 3]); // true
+    DOM.isFunction(function() {}); // true
+    DOM.isNumber('numero'); // false
+
+    Crie os seguintes métodos para verificação de tipo:
+    - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
+    O método isNull deve retornar `true` se o valor for null ou undefined.
+    */
+    function DOM(nodeDOM){
+        this.element = doc.querySelectorAll(nodeDOM);
+    };
+
+    function is(obj){
+        return Object.prototype.toString.call(obj);
+    };
+    
+    DOM.prototype.on = addEvents;
+    DOM.prototype.off = removeEvents;
+    DOM.prototype.get = getDomElements;
+    DOM.prototype.forEach = forEach;
+    DOM.prototype.map = map;
+    DOM.prototype.filter = filter;
+    DOM.prototype.reduce = reduce;
+    DOM.prototype.reduceRight = reduceRight;
+    DOM.prototype.every = every;
+    DOM.prototype.some = some;
+    DOM.prototype.isArray = isArray;
+    DOM.prototype.isObject = isObject;
+    DOM.prototype.isNumber = isNumber;
+    DOM.prototype.isFunction = isFunction;
+    DOM.prototype.isBoolean = isBoolean;
+    DOM.prototype.isString = isString;
+    DOM.prototype.isNull = isNull;
+
+    
+    function addEvents(event, callback){
+        this.element.forEach(function(item){
+          item.addEventListener(event, callback, false);
+        });
+    };
+    
+    function removeEvents(event, callback){
+        this.element.forEach(function(item){
+          item.removeEventListener(event, callback, false);
+        });
+    };
+    
+    function getDomElements(){
+        return this.element;
+    };
+
+    function forEach(){
+        return Array.prototype.forEach.apply(this.element, arguments)
+    };
+
+    function map(){
+        return Array.prototype.map.apply(this.element, arguments)
+    };
+
+    function filter(){
+        return Array.prototype.filter.apply(this.element, arguments)
+    };
+
+    function reduce(){
+        return Array.prototype.reduce.apply(this.element, arguments)
+    };
+
+    function reduceRight(){
+        return Array.prototype.reduceRight.apply(this.element, arguments)
+    };
+
+    function every(){
+        return Array.prototype.every.apply(this.element, arguments)
+    };
+
+    function some(){
+        return Array.prototype.some.apply(this.element, arguments)
+    };
+    
+    function isArray(obj){
+        return is(obj) === '[object Array]';
+    };
+
+    function isObject(obj){
+        return is(obj) === '[object Object]';
+    };
+
+    function isNumber(obj){
+        return is(obj) === '[object Number]';
+    };
+
+    function isFunction(obj){
+        return is(obj) === '[object Function]';
+    };
+
+    function isBoolean(obj){
+        return is(obj) === '[object Boolean]';
+    };
+
+    function isString(obj){
+        return is(obj) === '[object String]';
+    };
+
+    function isNull(obj){
+        return is(obj) === '[object Undefined]' || is(obj) === '[object Null]';
+    };
+    
+    console.log(DOM.prototype.isFunction());
+
+})(document);
