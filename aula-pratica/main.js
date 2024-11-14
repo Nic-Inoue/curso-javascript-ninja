9bb84ff2e998aa834f2568d083f034d93be1b5e9
Nicolas <nicolasinouealcantara@gmail.com>
2024-11-14 10:55:57 -0300

    Challenge 31 feito

--- aula-pratica/main.js
+++ aula-pratica/main.js
@@ -1,25 +1,5 @@
 (function(win, doc){
     'use strict';
+    console.log(Math.ceil(Math.random()*3));
 
-    var ajax = new XMLHttpRequest();
-    ajax.open('GET', 'data.json');
-    ajax.send(null); 
-
-    console.log('CarregandOOO...');
-    var response = '';
-    ajax.addEventListener('readystatechange', function(){
-        if(isRequestOk()){
-            try{
-                response = JSON.parse(ajax.responseText);
-            }
-            catch(e){
-                response = ajax.responseText;
-            }
-            console.log(response);
-        };
-    }, false);
-
-    function isRequestOk(){
-        return ajax.readyState === 4 && ajax.status === 200;
-    };
 })(window, document);
--- challenge-31/challenge-31.js
+++ challenge-31/challenge-31.js
@@ -11,4 +11,122 @@ Faça um pull request, também com a branch `challenge-31`, mas no repositório
 do curso, para colar o link do pull request do seu repo.
 */
 
-console.log('Link do pull request do seu projeto');
+(function($, doc) {
+    'use strict';
+
+    console.log('Link do pull request do seu projeto');
+  
+    function app(){
+      return {
+        init: function(){
+          let inputImage = $('[data-js="inputImage"]');
+          let inputModel = $('[data-js="inputModel"]');
+          let inputPlate = $('[data-js="inputPlate"]');
+          let inputColor = $('[data-js="inputColor"]');
+          let inputYear = $('[data-js="inputYear"]');
+          let buttonSubmit = $('button');
+          
+          buttonSubmit.on('click', function(event){
+            event.preventDefault();
+            let isInputImageValid = isInputValid(/\w+(?::\/\/)?\w+\.\w+(?:\.)?\w+.+?[jjp][ppn][geg][g]?$/g, inputImage);
+            let isInputModelValid = isInputValid(/^\w+-\w+$/g, inputModel);
+            let isInputPlateValid = isInputValid(/^[a-zA-Z]{3}-\d[\da-zA-Z]\d\d$/g, inputPlate);
+            let isInputYearValid = isInputValid(/^[12][09]\d\d$/g, inputYear);
+            let isInputColorValid = isInputValid(/^[A-Za-z]+$/g, inputColor);
+            verifyInput(isInputImageValid, inputImage);
+            verifyInput(isInputModelValid, inputModel);
+            verifyInput(isInputPlateValid, inputPlate);
+            verifyInput(isInputYearValid, inputYear);
+            verifyInput(isInputColorValid, inputColor);
+            if(isAllInputsValid(isInputImageValid, isInputModelValid, isInputPlateValid, isInputYearValid, isInputColorValid))
+              appendCarInfoIntoList();
+          });
+        
+          function appendCarInfoIntoList(){
+            let newLiCarInfo = doc.createElement('li');
+            let arrayInputCarInfo = [inputModel.get().value, inputPlate.get().value, inputYear.get().value, inputColor.get().value];
+            let newImgCar = doc.createElement('img');
+            let newRemoveButton = doc.createElement('button');
+            appendRemoveEventToButton(newRemoveButton);
+            appendAtributtesToImage(newImgCar, inputImage.get().value);
+            newLiCarInfo.appendChild(newImgCar);
+            for(var i = 0 ; i < 4 ; i++){
+              let newTextCarInfo = doc.createTextNode(arrayInputCarInfo[i]);
+              let newPCarInfo = doc.createElement('p');
+              let carList = $('[data-js="carsList"]');
+              newPCarInfo.appendChild(newTextCarInfo);
+              newLiCarInfo.appendChild(newPCarInfo);
+              carList.get().appendChild(newLiCarInfo);
+            };
+            newLiCarInfo.appendChild(newRemoveButton);
+          };
+
+          function appendRemoveEventToButton(newButton){
+            newButton.type = "submit";
+            newButton.appendChild(doc.createTextNode('Remover'));
+            newButton.addEventListener('click', function(e){
+                e.preventDefault();
+                console.log(this.parentElement.remove());
+            }, false)
+          };
+        
+          function appendAtributtesToImage(newImg, imgLink){
+            newImg.src = imgLink;
+            newImg.style.height = '100px';
+            newImg.style.width = '200px';
+          }
+        
+          function isAllInputsValid(isInput1, isInput2, isInput3, isInput4, isInput5){
+            return isInput1 && isInput2 && isInput3 && isInput4 && isInput5;
+          };
+        
+          function verifyInput(isInputValidBoolean, inputName){
+            if(!isInputValidBoolean){
+              let newDivError = doc.createElement('h3');
+              let newContentError = doc.createTextNode('Insira um valor válido.');
+              let form = $('[data-js="form"]');
+              newDivError.appendChild(newContentError);
+              form.get().insertBefore(newDivError, inputName.get());
+            };
+          };
+        
+          function isInputValid(regexCode, inputName){
+            let getRegexCode = regexCode;
+            if(inputName === inputYear){
+              let yearNumber = +(inputName.get().value);
+              if(getRegexCode.test(yearNumber))
+                return yearNumber > 1950 && yearNumber < 2025;
+            }
+            return getRegexCode.test(inputName.get().value);
+          };
+        },
+  
+        companyInfo: function companyInfo(){
+          let ajax = new XMLHttpRequest();
+          ajax.open('GET', 'company.json', true);
+          ajax.send();
+          ajax.addEventListener('readystatechange', this.getCompanyInfo, false);
+        },
+  
+        getCompanyInfo: function getCompanyInfo(){
+          if(!app().isReady.call(this))
+            return;
+          let data = JSON.parse(this.responseText);
+          let companyName = $('[data-js="company-name"]');
+          let companyPhone = $('[data-js="company-phone"]');
+          companyName.get().textContent = data.name;
+          companyPhone.get().textContent = data.phone;
+        },
+  
+        isReady: function isReady(){
+          return this.readyState === 4 && this.status === 200;
+        }
+      }
+    }
+  
+    app().init();
+    app().companyInfo();
+  
+  
+  })(window.DOM, document);
+  
